c61938739a8a8c4c9d04c7b3f3d4309c8a1e8183
Ryan <ryan2.lin@hwacom.com>
2023-01-12 22:51:47 +0800

    HWA-14 Logout BTN

--- lib/service/authenticate/auth.dart
+++ lib/service/authenticate/auth.dart
@@ -52,37 +52,14 @@ class AuthService{
 
     } on DioError catch (e) {
       return e.response!.data;
-      print(e);
-      return json.decode(e.response!.data);
     } on TimeoutException catch (e) {
-      //return {"success":false, "message":"{error:'${e.message}'}"};
       var errorMsg = tr('auth.connection_error');
       return {"response_code": 400, "success": false, "data": null, "message": '{"error":"$errorMsg"}'};
-      // if (e.response != null) {
-      //   print(e.response?.data);
-      //   print(e.response?.headers);
-      //   print(e.response?.requestOptions);
-      // } else {
-      //   // Something happened in setting up or sending the request that triggered an Error
-      //   print(e.requestOptions);
-      //   print(e.message);
-      // }
-      //
-      // if(e.type == DioErrorType.connectTimeout){
-      //   print("Connection  Timeout Exception");
-      //   Authenticated['message'] = "{error:'456123'}";
-      //   return Authenticated;
-      //   throw Exception("Connection  Timeout Exception");
-      // }
-      // print("Connection  789 Exception");
-      // Authenticated['message'] = "{error:'789456'}";
-      // return Authenticated;
-      // throw Exception(e.message);
     }
   }
 
   ///登出 todo:登出
-  Future<Response> logout(String accessToken) async {
+  Future<Map<String, dynamic>> logout(String accessToken) async {
     try {
       Response response = await dio.get(
         'http://10.0.2.2/api/logout',
--- lib/ui/widgets/header.dart
+++ lib/ui/widgets/header.dart
@@ -1,5 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hws_app/cubit/user_cubit.dart';
+import 'package:ionicons/ionicons.dart';
 
 class Header extends StatelessWidget {
   const Header({super.key, required this.text});
@@ -8,16 +11,46 @@ class Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 2, right: 2, top: 48, bottom: 24),
-      child: Text(
-        text,
-        textAlign: TextAlign.start,
-        style: Theme.of(context)
-            .textTheme
-            .headlineMedium!
-            .apply(fontWeightDelta: 2),
-      ),
+    return BlocBuilder<UserCubit, UserState>(
+        builder: (BuildContext context, UserState user){
+          return Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 2, right: 2, top: 48, bottom: 24),
+                child: Text(
+                  text,
+                  textAlign: TextAlign.start,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineMedium!
+                      .apply(fontWeightDelta: 2),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 2, right: 2, top: 48, bottom: 24),
+                child: TextButton.icon(
+                  onPressed: () {
+                    ///clear hydrated bloc state
+                    BlocProvider.of<UserCubit>(context).clearUser();
+                  },
+                  icon:Icon(Ionicons.log_out_outline, color: Theme.of(context).colorScheme.primary),
+                  label: Text(
+                    '',
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium!
+                        .apply(fontWeightDelta: 2, fontSizeDelta: -2),
+                  ),
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.red,
+                  ),
+                ),
+              ),
+            ],
+          );
+        }
     );
   }
 }
